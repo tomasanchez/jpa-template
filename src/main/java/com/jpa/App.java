c8479a714ad76ad88befbf7efaadd7db5735f6bb
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-11-17 00:38:33 -0300

    chore: first commit

---
+++ src/main/java/com/jpa/App.java
@@ -0,0 +1,13 @@
+package com.jpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
