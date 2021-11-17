10cf03c9dc0c002e3ba8a8eeaf2103b558a42ea7
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-11-17 11:17:21 -0300

    docs(mvc): model

--- src/main/java/com/jpa/core/mvc/model/Model.java
+++ src/main/java/com/jpa/core/mvc/model/Model.java
@@ -6,23 +6,43 @@ import java.util.Properties;
 import java.util.stream.Collectors;
 import com.google.gson.Gson;
 
+/**
+ * A model implementation for the TS-JPA Model-View-Controller concept.
+ * 
+ * @author Tomás Sánchez
+ * @since 1.0
+ */
 public class Model {
 
     private Map<String, Object> model = new HashMap<>();
 
     public Model() {}
 
+    /**
+     * Creates a new instance of a model from a Properties file object.
+     * 
+     * @param properties the properties file object
+     */
     public Model(Properties properties) {
         model = properties.entrySet().stream()
                 .collect(Collectors.toMap(e -> String.valueOf(e.getKey()),
                         e -> String.valueOf(e.getValue()), (prev, next) -> next, HashMap::new));
     }
 
+    /**
+     * Creates a new instance of a model from a JSON.
+     * 
+     * @param json a JSON string.
+     */
     @SuppressWarnings("unchecked")
     public Model(String json) {
         Gson gson = new Gson();
         model = gson.fromJson(json, Map.class);
     }
+
+    /* =========================================================== */
+    /* Getters & Setter ------------------------------------------ */
+    /* =========================================================== */
 
     /**
      * Gets the value of the property.
