0f2df3a12a845af4cc8f31bf8112330253f66574
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-11-17 10:22:44 -0300

    docs(mvc): controller

--- src/main/java/com/jpa/core/mvc/controller/Controller.java
+++ src/main/java/com/jpa/core/mvc/controller/Controller.java
@@ -5,7 +5,14 @@ import com.jpa.core.mvc.view.View;
 import spark.Request;
 import spark.Response;
 
+/**
+ * A generic controller implementation for the TS-JPA Model-View-Controller concept.
+ * 
+ * @author Tomás Sánchez
+ * @since 1.0
+ */
 public abstract class Controller {
+
     private static Model sharedModel = new Model();
     private View view;
 
@@ -14,51 +21,90 @@ public abstract class Controller {
         this.onInit();
     }
 
+    /* =========================================================== */
+    /* Getters & Setter ------------------------------------------ */
+    /* =========================================================== */
+
+    /**
+     * Gets the shared model among controllers.
+     * 
+     * @return A shared model
+     */
     public static Model getSharedModel() {
         return sharedModel;
     }
 
-    public static void setSharedModel(Model sharedModel) {
+    /**
+     * Sets a shared model among all controllers.
+     * 
+     * @param sharedModel a model to be shared between controllers
+     * @return the shared model
+     */
+    public static Model setSharedModel(Model sharedModel) {
         Controller.sharedModel = sharedModel;
+        return getSharedModel();
     }
 
+    /**
+     * Retrieves the current controller view.
+     * 
+     * @return the view of the current controller
+     */
     public View getView() {
         return view;
     }
 
-    public void setView(View view) {
+    /**
+     * Sets a controller view.
+     * 
+     * @param view
+     * @return
+     */
+    public Controller setView(View view) {
         this.view = view;
+        return this;
     }
 
     /**
-     * Obtiene el nombre del controlador.
+     * Obtains the controller name.
      * 
-     * ? Ej: HomeController => home
+     * ? Example: HomeController => home
      * 
-     * @return el nombre del controller
+     * @return the controller name
      */
     public String getName() {
         return getClass().getSimpleName().toLowerCase().replace("controller", "");
     }
 
+    /* =========================================================== */
+    /* Lifecycle methods ----------------------------------------- */
+    /* =========================================================== */
+
     /**
-     * Funcion llamada por única vez, antes de crear el controllador.
+     * This method is called upon initialization of the View. The controller can perform its
+     * internal setup in this hook. It is only called once per View instance, unlike the
+     * onBeforeRendering and onAfterRendering hooks.
      */
     protected abstract void onInit();
 
     /**
-     * Funcion llamada siempre antes de renderizar la Vista.
      * 
-     * @param request la HTTP request.
-     * @param response la HTTP response.
+     * This method is called every time the View is rendered, before the Renderer is called and the
+     * HTML is placed in the DOM-Tree. It can be used to perform clean-up-tasks before re-rendering.
+     * 
+     * @param request the Spark HTTP request object
+     * @param response the Spark HTTP response object
      */
     protected abstract void onBeforeRendering(Request request, Response response);
 
     /**
-     * Método llamado despues de renderizar la vista.
      * 
-     * @param request la spark request
-     * @param response la spark response
+     * This method is called every time the View is rendered, after the HTML is placed in the
+     * DOM-Tree. It can be used to apply additional changes to the Model after the Renderer has
+     * finished.
+     * 
+     * @param request the spark HTTP request object
+     * @param response the spark HTTP response object
      */
     protected abstract void onAfterRendering(Request request, Response response);
 
