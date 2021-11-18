a07ff5248b2bf1f556303a0f053434818f51edcf
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-11-18 14:23:05 -0300

    feat(controller): base convenience

--- src/main/java/com/jpa/controller/BaseController.java
+++ src/main/java/com/jpa/controller/BaseController.java
@@ -1,12 +1,32 @@
 package com.jpa.controller;
 
+import java.util.Map;
 import com.jpa.core.mvc.controller.Controller;
+
 
 public abstract class BaseController extends Controller {
 
 
     /* =========================================================== */
-    /* Internal Methods ------------------------------------------ */
+    /* Convenience Methods --------------------------------------- */
     /* =========================================================== */
 
+    /**
+     * Convenience method for getting the view model map.
+     * 
+     * @return the View Model
+     */
+    public Map<String, Object> getModel() {
+        return this.getView().getModel().getData();
+    }
+
+    /**
+     * Convenience method for getting a value from i18n.
+     * 
+     * @param text the key value
+     * @return the text value of the key
+     */
+    public String getText(String text) {
+        return (String) Controller.getI18n().getText(text);
+    }
 }
