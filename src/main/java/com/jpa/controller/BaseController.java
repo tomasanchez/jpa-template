2a0687edf5d540352636b7415abbc9f2494fe30e
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-11-18 09:58:10 -0300

    refactor: unused methods

--- src/main/java/com/jpa/controller/BaseController.java
+++ src/main/java/com/jpa/controller/BaseController.java
@@ -1,18 +1,12 @@
 package com.jpa.controller;
 
 import com.jpa.core.mvc.controller.Controller;
-import spark.Request;
-import spark.Response;
 
 public abstract class BaseController extends Controller {
 
-    /* =========================================================== */
-    /* HTTP Request Handling ------------------------------------- */
-    /* =========================================================== */
 
-    @Override
-    protected Object getResponse(Request request, Response response) {
-        return null;
-    }
+    /* =========================================================== */
+    /* Internal Methods ------------------------------------------ */
+    /* =========================================================== */
 
 }
