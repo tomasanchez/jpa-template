1d57be4e6904c4b7bfe50d662a2cf329e77c0026
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-11-18 23:30:51 -0300

    feat(mvc): login

---
+++ src/main/java/com/jpa/controller/LogInController.java
@@ -0,0 +1,26 @@
+package com.jpa.controller;
+
+import spark.Request;
+import spark.Response;
+
+public class LogInController extends BaseController {
+
+    @Override
+    protected void onInit() {
+        // TODO Auto-generated method stub
+        this.getEndPoint();
+    }
+
+    @Override
+    protected void onBeforeRendering(Request request, Response response) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected void onAfterRendering(Request request, Response response) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
