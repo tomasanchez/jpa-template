6653285ad2319bdc5945e0356154d83fc22140ac
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-02-28 15:29:17 -0300

    feat: add media type constants

---
+++ src/main/java/com/jpa/core/utils/MediaType.java
@@ -0,0 +1,22 @@
+package com.jpa.core.utils;
+
+public class MediaType {
+
+    public static final String APPLICATION_JSON = "application/json";
+
+    public static final String IMAGE_GIF = "image/gif";
+
+    public static final String IMAGE_JPEG = "image/jpeg";
+
+    public static final String IMAGE_PNG = "image/png";
+
+    public static final String MULTIPART_FORM_DATA = "multipart/form-data";
+
+    public static final String MULTIPART_MIXED = "multipart/mixed";
+
+    public static final String TEXT_HTML = "text/html";
+
+    public static final String TEXT_MARKDOWN = "text/markdown";
+
+    public static final String TEXT_PLAIN = "text/plain";
+}
