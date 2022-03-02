0e3273b09d36cf072b238d29b68c4adc2addc466
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-03-02 11:39:12 -0300

    refactor: use of BCrypt for password

--- src/main/java/com/jpa/app/Bootstrap.java
+++ src/main/java/com/jpa/app/Bootstrap.java
@@ -1,5 +1,6 @@
 package com.jpa.app;
 
+import com.jpa.core.security.crypto.BCryptPasswordEncoder;
 import com.jpa.model.user.User;
 import com.jpa.repositories.UserRepository;
 import org.uqbarproject.jpa.java8.extras.EntityManagerOps;
@@ -19,7 +20,7 @@ public class Bootstrap implements WithGlobalEntityManager, EntityManagerOps, Tra
      * Initializates an admin user.
      */
     public void bootstrapData() {
-        User admin = new User("admin", "admin");
+        User admin = new User("admin", new BCryptPasswordEncoder().encode("admin"));
 
         UserRepository repository = new UserRepository();
 
